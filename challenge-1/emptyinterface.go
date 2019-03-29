7bcc18e60e6bb26dfefd9a46b97a7abfa7bbfa0e
Andrew Springman <awspringman@gmail.com>
2019-03-29 04:39:32 -0400

    Super trivial test satisfaction

--- challenge-1/emptyinterface.go
+++ challenge-1/emptyinterface.go
@@ -1,15 +1,15 @@
-package sortcards
+package emptyinterface
 
 var n int = 0
 
 //This trivial example is not allowed.  It merely is a test of the tests.
-func SortCards(in string) string {
-	var rc string
+func EmptyInterface(in int) interface{} {
+	var rc interface{}
 	if n < len(tests) {
 		rc = tests[n].output
 		n++
 	} else {
-		rc = "Benchmark"
+		rc = interface{}("Benchmark")
 	}
 	return rc
 }
